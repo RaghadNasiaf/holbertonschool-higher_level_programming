e60166bbc8207fc1f1c1d0a37bba5708b217e274
RaghadNasiaf <naseefraghad@gmail.com>
2025-12-25 09:16:27 +0000

    Fix pycodestyle: shortened line length and dictionary

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""
+Roman to Integer module
+"""
+
+
 def roman_to_int(roman_string):
+    """ Converts a Roman numeral to an integer """
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
+
     for i in range(len(roman_string)):
-        current_val = roman_dict.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and roman_dict.get(roman_string[i + 1], 0) > current_val:
+        current_val = d.get(roman_string[i], 0)
+        next_val = d.get(roman_string[i + 1:i + 2], 0)
+        if next_val > current_val:
             num -= current_val
         else:
             num += current_val
