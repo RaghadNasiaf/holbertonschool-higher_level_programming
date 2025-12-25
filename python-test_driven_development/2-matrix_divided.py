6fc21e744a4ac8bf60321bfb023eb8f33e3066e9
RaghadNasiaf <naseefraghad@gmail.com>
2025-12-25 10:09:44 +0000

    Task 1: Matrix division with TDD and full documentation

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module provides a function that divides all elements of a matrix.
+Each element is rounded to 2 decimal places.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by a divisor.
+    Args:
+        matrix: A list of lists of integers or floats.
+        div: The number to divide by.
+    Returns:
+        A new matrix with the results.
+    """
+    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    
+    if not isinstance(matrix, list) or not matrix or not matrix[0]:
+        raise TypeError(err_msg)
+
+    row_len = len(matrix[0])
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(err_msg)
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        for x in row:
+            if not isinstance(x, (int, float)):
+                raise TypeError(err_msg)
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(x / div, 2) for x in row] for row in matrix]
