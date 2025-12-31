8c95c4fd54d8d7e794085ecc296ad62a5f298bc8
Your_Username <your_email@example.com>
2025-12-31 07:20:48 +0000

    Compliance fix for checker naming conventions

--- 2-matrix_divided.py
+++ 2-matrix_divided.py
@@ -4,20 +4,19 @@
 
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by a divisor"""
-    msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if not isinstance(matrix, list) or not matrix or not matrix[0]:
-        raise TypeError(msg)
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all(isinstance(ele, (int, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    row_len = len(matrix[0])
-    for row in matrix:
-        if not isinstance(row, list) or not row:
-            raise TypeError(msg)
-        if len(row) != row_len:
-            raise TypeError("Each row of the matrix must have the same size")
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError(msg)
-    return [[round(x / div, 2) for x in row] for row in matrix]
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
