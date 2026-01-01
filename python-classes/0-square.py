504865a296906a9b6cbfc69910a1fbd38c1bc53f
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-01 08:25:41 +0000

    Simplified docstrings for check 7

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""
-This module provides a simple class Square to define a geometric square.
-It is part of the Holberton School foundation program for Python OOP.
-"""
+"""Defines a class Square."""
 
 
 class Square:
-    """
-    A class used to represent a Square.
-
-    Attributes:
-        No attributes are defined for this empty class yet.
-    """
+    """Represent a square."""
     pass
-
