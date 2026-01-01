dd3cbebbca09ccd1cb4a42e94a7ec51c2076207b
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-01 08:08:42 +0000

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-Module 0-square: Introduction to Python classes.
-This module provides a simple, empty Square class that will be
-the foundation for subsequent tasks in the Holberton curriculum.
-Author: RaghadNasiaf <nasdeefraghad@gmail.com>
+This module defines an empty class Square.
 """
 
 
 class Square:
     """
-    An empty class Square that defines a square.
-    This class does not have any attributes or methods yet.
-    The purpose is to understand the basic syntax of class definition.
+    An empty class that defines a square.
     """
     pass
