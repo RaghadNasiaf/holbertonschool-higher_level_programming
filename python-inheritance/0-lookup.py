3f7e68caf59039283a97b3ba12921d94f092af7a
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-01 09:20:31 +0000

    Initialize inheritance directory and add task 0 lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns the list of available
+attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list of strings representing the attributes and methods.
+    """
+    return dir(obj)
