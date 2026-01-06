6f5d0d51fa539b2dbf2e0085b5e1a1c31992ee47
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-06 10:21:29 +0000

    Complete and correct task 7 with PEP8 and tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ Contains class BaseGeometry
 
 class BaseGeometry:
     """
-    A class BaseGeometry
+    A class BaseGeometry with public instance methods area and integer_validator
     """
     def area(self):
         """
@@ -18,6 +18,12 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validates the value
+        Args:
+            name (str): name of the value
+            value (int): value to validate
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is <= 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
