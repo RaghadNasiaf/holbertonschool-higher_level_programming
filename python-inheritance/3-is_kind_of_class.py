8c82979e916ef0a2037abc27e3afc2bcfdba6707
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-06 08:32:45 +0000

    Fix directory structure: remove nested python-inheritance folder

--- python-inheritance/3-is_kind_of_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module for is_kind_of_class function"""
-
-
-def is_kind_of_class(obj, a_class):
-    """Returns True if object is an instance of, or inherited from, a_class"""
-    return isinstance(obj, a_class)
