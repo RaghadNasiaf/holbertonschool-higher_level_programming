916b6893edb6837e5739e708d4553ac492e118a6
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-06 07:59:27 +0000

    Add task 5 and 6: BaseGeometry enhancements

---
+++ 6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    A class with an area method
+    """
+    def area(self):
+        """
+        Raises an Exception with a message
+        """
+        raise Exception("area() is not implemented")
