4980df1231938b8f79dad1332a649d0af0a0f0bf
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-07 11:12:56 +0000

    Fix variable naming in Circle and exact formatting

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -29,7 +29,7 @@ class Circle(Shape):
 
     def area(self):
         """Return the area of the circle."""
-        return math.pi * self.radius ** 2
+        return math.pi * (self.radius ** 2)
 
     def perimeter(self):
         """Return the perimeter of the circle."""
