0eb591fdb0679adc0f3f4e5a9fbd4efa38742b9c
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-07 12:35:33 +0000

    Add Task 6: Function to load Python object from JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for Task 6: Loading an object from a JSON file.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+    Args:
+        filename (str): The name of the file to read from.
+    Returns:
+        object: The Python object represented by the JSON file.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
