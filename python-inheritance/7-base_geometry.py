58f28f693f96cdd9239bdcdaa12c15cc185c7c4e
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-07 08:04:42 +0000

    Final fix: accurate error messages and comprehensive tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,13 +11,13 @@ class BaseGeometry:
     """
     def area(self):
         """
-        Raises an Exception
+        Raises an Exception with the message area() is not implemented
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Validates value
+        Validates the value
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
