93cc76e458f53fbfebad2b731b7611f475d70910
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-07 12:56:13 +0000

    Fix Task 12: Remove whitespaces from blank lines (W293)

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -18,11 +18,11 @@ def pascal_triangle(n):
         prev_row = triangle[i - 1]
         # Each row starts with 1
         current_row = [1]
-        
+
         # Calculate middle elements based on the previous row
         for j in range(1, i):
             current_row.append(prev_row[j - 1] + prev_row[j])
-            
+
         # Each row ends with 1
         current_row.append(1)
         triangle.append(current_row)
