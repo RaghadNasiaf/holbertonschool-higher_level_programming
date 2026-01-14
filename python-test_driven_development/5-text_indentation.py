2f4debe7f9cfda0bb4fcf81d11e2b2bfc5fab65c
RaghadNasiaf <naseefraghad@gmail.com>
2026-01-14 06:25:59 +0000

    Final: Fixed Pycodestyle W293 (hidden whitespace in blank lines)

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,19 +12,16 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # Clean the string from start/end spaces
     text = text.strip()
-    
-    # Flags to manage spaces and punctuation
     skip_space = False
-    
+
     for i in range(len(text)):
         if skip_space and text[i] == ' ':
             continue
-        
+
         print(text[i], end="")
         skip_space = False
-        
+
         if text[i] in ".?:":
             print("\n")
             skip_space = True
